b8764dbf9359c84ba0d09db9e36fd35c27989edc
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-02-24 10:28:21 +0900

    added 2nd test from 28/63 https://ru.code-basics.com/languages/python/modules/variables/lessons/symbols

---
+++ 02_str_var_array.py
@@ -0,0 +1,8 @@
+one = 'Naharis'
+two = 'Mormont'
+three = 'Sand'
+
+# BEGIN
+template = ({})[{}]
+results = template.format(one,3)+template.format(two,2)+template.format(three,4)+template.format(two,5)+template.format(two,3)
+# END
