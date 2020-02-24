04186410e20050eb5b17f0fdd23ab8d2e4254995
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-02-24 20:08:22 +0900

    03 and 04 lessons done

---
+++ 03_print_jaimes_line.py
@@ -0,0 +1,6 @@
+def print_jaimes_line(line):
+    print(line)
+
+line='print'
+print_jaimes_line(line)
+
---
+++ 04_seq_lines.py
@@ -0,0 +1,6 @@
+def print_seq(line, number):
+    print (line * number)
+
+line = '-=='
+number = 5
+print_seq(line, number)
