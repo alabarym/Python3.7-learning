4a98b1c4790a5ac64013de9273b9cd8885dd2403
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-04-12 12:14:34 +0900

    017  correct code was written

--- 017_check_input_conditions.py
+++ 017_check_input_conditions.py
@@ -3,17 +3,17 @@ def is_arguments_for_substr_correct(string, index, string_length):
         return False
     elif string_length < 0:
         return False
-    elif index > string_length -1 :
+    elif index > len(string) -1 :
         return False
     elif len(string) < (string_length + index ) :
         return False     
-    else: 
+    else:
         return True   
   
 string = 'Sansa Stark'
 print(is_arguments_for_substr_correct(string, -1, 3))
-print(is_arguments_for_substr_correct(string, 4, 100)) 
-print(is_arguments_for_substr_correct(string, 10, 10)) 
-print(is_arguments_for_substr_correct(string, 11, 1))  
+print(is_arguments_for_substr_correct(string, 4, 100))
+print(is_arguments_for_substr_correct(string, 10, 10))
+print(is_arguments_for_substr_correct(string, 11, 1))
 print(is_arguments_for_substr_correct(string, 3, 3))  
 print(is_arguments_for_substr_correct(string, 0, 5))
