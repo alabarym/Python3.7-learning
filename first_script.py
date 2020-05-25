4c5527940292e41d3cd16df02f061db066c60353
Vladimir Vasilenko <vladimir.a.vasilenko@gmail.com>
2020-05-25 16:56:15 +0300

    first script update

---
+++ first_script.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+interface = input("Input interface = ")
+vlan = input("Input vlan = ")
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('\n' + '-' * 30)
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
+
